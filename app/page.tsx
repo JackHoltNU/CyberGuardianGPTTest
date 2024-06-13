3b12008766c3febd381f91ac37b76a7e71ac7d08
Jack Holt <jackholt@duck.com>
2024-06-13 19:21:24 +0100

    removes comments

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,11 @@ import { redirect } from "next/navigation";
 
 
 const Home = async () => {
-  // const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const session = await getServerSession(options);
 
   if (!session) {
     redirect('api/auth/signin?callbackUrl=%2F')
   }
-
-  // const toggleChat = useCallback((state: boolean) => {
-  //   // setLoggedIn(state);
-  // }, []);
 
   return (
       <main className="flex flex-col w-screen h-screen items-center">
