4447a9c3cf6a9eaf2b873415e02501b85f325455
Jack Holt <jack.holt.ext@bbc.co.uk>
2024-09-06 10:20:02 +0100

    adds api timeout - may fix timeout bug

--- app/api/sendMessageToChat/route.ts
+++ app/api/sendMessageToChat/route.ts
@@ -8,49 +8,52 @@ import { NextRequest, NextResponse } from "next/server";
 import AIConfig from "@/app/models/AIConfig";
 
 interface Props {
-    messageHistory: MessageHistory[], 
-    user: string, 
-    threadID: string | undefined
+  messageHistory: MessageHistory[];
+  user: string;
+  threadID: string | undefined;
 }
 
 interface ChatCompletionRequestMessage {
-    role: "system" | "user" | "assistant";
-    content: string;
+  role: "system" | "user" | "assistant";
+  content: string;
 }
-
 
 export const POST = async (req: Request) => {
   const body = await req.json();
-  const session = await getServerSession(options); 
+  const session = await getServerSession(options);
   let { messageHistory, user, threadID } = body as Props;
 
-  if(!session){
+  if (!session) {
     return new Response(`User not authenticated`, {
       status: 401,
-    })
+    });
   }
 
-  if(session.user?.name != user){
-
+  if (session.user?.name != user) {
     return new Response(`Correct user not authenticated`, {
-        status: 401,
-    })
+      status: 401,
+    });
   }
 
-  
   try {
     await connectToDatabase();
   } catch (error: any) {
     console.error("Couldn't connect to database");
-    throw new Error(error.message)
+    throw new Error(error.message);
   }
 
-  if(threadID === undefined){
+  if (threadID === undefined) {
     threadID = crypto.randomUUID();
   }
 
   // record user's message to database
-  await createOrContinueChat(threadID,"", user, messageHistory[messageHistory.length - 1], messageHistory);
+  await createOrContinueChat(
+    threadID,
+    "",
+    user,
+    messageHistory[messageHistory.length - 1],
+    messageHistory
+  );
 
   const config = await getAIConfig();
   let model: string;
@@ -59,14 +62,14 @@ export const POST = async (req: Request) => {
 
   let messagesParam: ChatCompletionRequestMessage[] = [];
 
-  if(config){
+  if (config) {
     model = config.primary;
     mainPrompt = config.mainPrompt;
     formatPrompt = config.formatPrompt;
     messagesParam = [
       {
         role: "system",
-        content: `${mainPrompt} ${formatPrompt}`
+        content: `${mainPrompt} ${formatPrompt}`,
       },
       ...messageHistory.map((item) => ({
         role: item.sender,
@@ -76,32 +79,33 @@ export const POST = async (req: Request) => {
       //   role: "user",
       //   content: formatPrompt
       // }
-    ]
-  } else {    
+    ];
+  } else {
     return new Response("Could not create chat completion", {
       status: 500,
-    })
+    });
   }
 
-  
-
   // get bot response
-  try {    
-    if(!config?.primary){
-      return new Response("Could not create chat completion due to missing model information", {
-        status: 500,
-      })
+  try {
+    if (!config?.primary) {
+      return new Response(
+        "Could not create chat completion due to missing model information",
+        {
+          status: 500,
+        }
+      );
     }
     let completion = await getCompletion(messagesParam, config.primary);
 
     let response: string = "";
     let title: string | undefined = "";
     let tags: string[] | undefined = [];
-    const emptyFeedback:MessageRating = {
+    const emptyFeedback: MessageRating = {
       upvoted: false,
       downvoted: false,
-      comments: []
-    }
+      comments: [],
+    };
 
     try {
       let responseMessage = completion.choices[0].message.content ?? "";
@@ -111,7 +115,7 @@ export const POST = async (req: Request) => {
 
       try {
         jsonResponse = JSON.parse(responseMessage);
-        if(jsonResponse.response){
+        if (jsonResponse.response) {
           successfulResponse = true;
         }
       } catch (error) {
@@ -119,57 +123,77 @@ export const POST = async (req: Request) => {
         successfulResponse = false;
       }
 
-      while(!successfulResponse && iterations < 3){
-        try {          
-          completion = await getCompletion(messagesParam, config.primary);
-          responseMessage = completion.choices[0].message.content ?? "";
-          jsonResponse = JSON.parse(responseMessage);
-          if(jsonResponse.response){
-            successfulResponse = true;
-          } else {
-            iterations ++;
-          }
-        } catch (error) {
-          console.error(`Failed to parse JSON message`);
-          iterations ++;
-        }                
-      }
-      
+      // while(!successfulResponse && iterations < 3){
+      //   try {
+      //     completion = await getCompletion(messagesParam, config.primary);
+      //     responseMessage = completion.choices[0].message.content ?? "";
+      //     jsonResponse = JSON.parse(responseMessage);
+      //     if(jsonResponse.response){
+      //       successfulResponse = true;
+      //     } else {
+      //       iterations ++;
+      //     }
+      //   } catch (error) {
+      //     console.error(`Failed to parse JSON message`);
+      //     iterations ++;
+      //   }
+      // }
+
       title = jsonResponse.title;
-      response = jsonResponse.response;      
+      response = jsonResponse.response;
       const tagsRaw: string = jsonResponse.tags;
-      if(tagsRaw){
+      if (tagsRaw) {
         tags = tagsRaw.split(",");
-      }      
+      }
     } catch (error: any) {
       console.error("Failed to parse JSON message, returning raw response");
       return new Response("Could not create chat completion", {
         status: 500,
-      })
+      });
     }
     const responseId = crypto.randomUUID();
 
-    await createOrContinueChat(threadID, title, user, {sender: "assistant", text: response, id: responseId, messageRating: emptyFeedback, model, mainPrompt, formatPrompt},messageHistory, tags);
+    await createOrContinueChat(
+      threadID,
+      title,
+      user,
+      {
+        sender: "assistant",
+        text: response,
+        id: responseId,
+        messageRating: emptyFeedback,
+        model,
+        mainPrompt,
+        formatPrompt,
+      },
+      messageHistory,
+      tags
+    );
 
     return Response.json({
-        id: responseId,
-        title: title,
-        message: response,
-        threadID: threadID,        
-        userTokens: completion.usage?.prompt_tokens,
-        botTokens: completion.usage?.completion_tokens,
-      })
-    
+      id: responseId,
+      title: title,
+      message: response,
+      threadID: threadID,
+      userTokens: completion.usage?.prompt_tokens,
+      botTokens: completion.usage?.completion_tokens,
+    });
   } catch (error: any) {
     console.error("Couldn't create chat completion", error);
     return new Response("Could not create chat completion", {
       status: 500,
-    })
+    });
   }
 };
 
-const getCompletion = async (messagesParam: ChatCompletionRequestMessage[], model: string) => {
-  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const getCompletion = async (
+  messagesParam: ChatCompletionRequestMessage[],
+  model: string
+) => {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+    timeout: 60000,
+  });
   // const model = "ft:gpt-3.5-turbo-1106:personal:test-finetune:9WLpJhZj";
   // const model = "gpt-4o-mini";
 
@@ -178,37 +202,44 @@ const getCompletion = async (messagesParam: ChatCompletionRequestMessage[], mode
     model,
     response_format: { type: "json_object" },
   });
-}
+};
 
 const getAIConfig = async () => {
   const aiConfig: AIConfigType | null = await AIConfig.findOne();
   return aiConfig;
-}
+};
 
-const createOrContinueChat = async (threadID: string, title: string | undefined, user: string, newMessage: MessageHistory, messageHistory: MessageHistory[], tags?: String[]) => {
-    const chat = await Chat.findOne({ threadID });
-  
-    try {
-      if (chat) {
-          chat.messages.push(newMessage);
-          if(title){
-            chat.title = title;
-          }
-          chat.latestTimestamp = Date.now();
-          if(tags){
-            chat.tags = tags;
-          }          
-          await chat.save();
-        } else {
-          await Chat.create({
-            threadID,
-            title,
-            user,
-            messages: [...messageHistory],
-          });
-        }
-    } catch (error: any) {
-      console.error(`Couldn't save chat to database`)
-      throw new Error(error.message);
-    }  
+const createOrContinueChat = async (
+  threadID: string,
+  title: string | undefined,
+  user: string,
+  newMessage: MessageHistory,
+  messageHistory: MessageHistory[],
+  tags?: String[]
+) => {
+  const chat = await Chat.findOne({ threadID });
+
+  try {
+    if (chat) {
+      chat.messages.push(newMessage);
+      if (title) {
+        chat.title = title;
+      }
+      chat.latestTimestamp = Date.now();
+      if (tags) {
+        chat.tags = tags;
+      }
+      await chat.save();
+    } else {
+      await Chat.create({
+        threadID,
+        title,
+        user,
+        messages: [...messageHistory],
+      });
+    }
+  } catch (error: any) {
+    console.error(`Couldn't save chat to database`);
+    throw new Error(error.message);
   }
+};
