248c817fdff07cfa0890c35f162271e71fbd567c
Jack Holt <jackholt@duck.com>
2025-04-22 19:30:38 +0100

    keyboard tweak

--- app/dualChat/dualchat.tsx
+++ app/dualChat/dualchat.tsx
@@ -561,7 +561,7 @@ const DualChatbotInterface = ({ session }: Props) => {
             style={{
               height:
                 keyboardVisible && isFullScreen
-                  ? `calc(100vh - ${keyboardHeight}px)`
+                  ? `calc(100vh - ${keyboardHeight}px - 100px)`
                   : "auto",
             }}
           >
@@ -682,7 +682,7 @@ const DualChatbotInterface = ({ session }: Props) => {
             style={{
               height:
                 keyboardVisible && isFullScreen
-                  ? `calc(100vh - ${keyboardHeight}px)`
+                  ? `calc(100vh - ${keyboardHeight}px - 100px)`
                   : "auto",
             }}
           >
